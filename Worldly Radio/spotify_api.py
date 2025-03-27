3fdf7fc62a7f9c209b8a02af76ae6f1d217c7ec3
Isaac Hopwood <isaac.hopwood@higg.com>
2025-03-27 16:19:51 -0500

    create spotify_api.py

---
+++ Worldly Radio/spotify_api.py
@@ -0,0 +1,14 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Set up authentication
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id="dcbfa82eb9454afabd02e143dbe2cfca",
+    client_secret="a399bffdd7114e339b41a9ed3d044fb2",
+    redirect_uri="http://localhost:8888/callback",
+    scope="playlist-read-private"
+))
+
+# Get user ID
+user_id = sp.me()["id"]
+print(f"Connected as: {user_id}")
