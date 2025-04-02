1bd6a47502b04f4aef7a38d5b5e3380babf12f35
Isaac Hopwood <isaac.hopwood@higg.com>
2025-04-02 09:01:36 -0500

    Create FDM calc accounts impacted.sql

---
+++ SQL/FDM calc accounts impacted.sql
@@ -0,0 +1,32 @@
+SELECT
+COUNT(DISTINCT account_id)
+-- account_id,
+-- raw -> 'status' AS status,
+-- CASE WHEN raw ->> 'status' LIKE '%VRF%' 
+-- 		THEN raw ->'verifierData'->>'sipfacilitytype'
+-- 		ELSE raw ->'facilityData'->>'sipfacilitytype' END AS facility_type,
+-- CASE WHEN raw ->> 'status' LIKE '%VRF%' 
+-- 		THEN raw ->'verifierData'->>'ensourceelectricmetertrack'
+-- 		ELSE raw ->'facilityData'->>'ensourceelectricmetertrack' END AS meters,
+-- CASE WHEN raw ->> 'status' LIKE '%VRF%' 
+-- 		THEN raw ->'verifierData'->>'ensourcetracksepdomprod'
+-- 		ELSE raw ->'facilityData'->>'ensourcetracksepdomprod' END AS domprodsep
+FROM public.dct
+WHERE
+	CASE WHEN raw ->> 'status' LIKE '%VRF%' 
+		THEN raw ->'verifierData'->>'ensourceelectricmetertrack'
+		ELSE raw ->'facilityData'->>'ensourceelectricmetertrack' END LIKE 'no'
+	AND CASE WHEN raw ->> 'status' LIKE '%VRF%' 
+		THEN raw ->'verifierData'->>'sipfacilitytype'
+		ELSE raw ->'facilityData'->>'sipfacilitytype' END LIKE '%finalProductAssembly%'
+	AND CASE WHEN raw ->> 'status' LIKE '%VRF%' 
+		THEN raw ->'verifierData'->>'ensourcetracksepdomprod'
+		ELSE raw ->'facilityData'->>'ensourcetracksepdomprod' END LIKE 'yes'	
+	AND (CASE WHEN raw ->> 'status' LIKE '%VRF%' 
+		THEN raw ->'verifierData'->>'energyseparate'
+		ELSE raw ->'facilityData'->>'energyseparate' END NOT LIKE 'no'
+		OR CASE WHEN raw ->> 'status' LIKE '%VRF%' 
+		THEN raw ->'verifierData'->>'energyseparate'
+		ELSE raw ->'facilityData'->>'energyseparate' END IS NULL)
+	AND raw ->> 'surveyVersion' = '2.0.0'
+	AND raw ->> 'facilityPosted' = 'true'
