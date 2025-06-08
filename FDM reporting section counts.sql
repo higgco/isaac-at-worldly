1159d5587e96b2a0a830d2417ef52a9a4a40be63
Isaac Hopwood <isaac.hopwood@higg.com>
2025-06-08 17:03:07 -0500

    Create FDM reporting section counts.sql

---
+++ FDM reporting section counts.sql
@@ -0,0 +1,38 @@
+WITH total AS (
+    SELECT
+        COUNT(DISTINCT assessment_id) AS total_count
+    FROM public.dct
+    WHERE (raw ->> 'facilityPosted')::boolean = TRUE
+),
+
+three AS (
+    SELECT
+        COUNT(DISTINCT assessment_id) AS three_count
+    FROM public.dct
+    WHERE
+        raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' ILIKE '%production%'
+        AND raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' ILIKE '%energyuse%'
+        AND raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' ILIKE '%wateruse%'
+        AND raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' NOT ILIKE '%waste%'
+        AND raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' NOT ILIKE '%wastewater%'
+        AND raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' NOT ILIKE '%airemissions%'
+),
+
+four AS (
+    SELECT
+        COUNT(DISTINCT assessment_id) AS four_count
+    FROM public.dct
+    WHERE
+        raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' ILIKE '%production%'
+        AND raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' ILIKE '%energyuse%'
+        AND raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' ILIKE '%wateruse%'
+        AND raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' ILIKE '%waste%'
+        AND raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' NOT ILIKE '%wastewater%'
+        AND raw -> 'results' -> 'answers' -> 'report_sections' ->> 'response' NOT ILIKE '%airemissions%'
+)
+
+SELECT 
+    total.total_count,
+    three.three_count,
+    four.four_count
+FROM total, three, four;
