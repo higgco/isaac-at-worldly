074f0ad14b2c14f8deb8c3612bb2b4ba49ed1e86
Isaac Hopwood <isaac.hopwood@higg.com>
2025-07-08 16:57:07 -0500

    Update FDM-PIC linking meta queries.sql

--- SQL/FDM-PIC linking meta queries.sql
+++ SQL/FDM-PIC linking meta queries.sql
@@ -1,9 +1,13 @@
 SELECT
-	assessment_id,
+	COUNT (DISTINCT assessment_id) AS count,
 	account_id,
-	raw ->> 'status' AS status,
-	raw ->> 'facilityPosted' AS facility_posted,
-	raw -> 'results' -> 'answers' -> 'reportingmonth' ->> 'response' AS reporting_month,
 	raw -> 'results' -> 'answers' -> 'reportingyear' ->> 'response' AS reporting_year
+	-- raw ->> 'status' AS status,
+	-- (raw ->> 'facilityPosted')::boolean AS facility_posted,
+	-- (raw ->> 'verifierPosted')::boolean AS verifier_posted,
+	-- raw -> 'results' -> 'answers' -> 'reportingmonth' ->> 'response' AS reporting_month,
+	-- raw -> 'results' -> 'answers' -> 'reportingyear' ->> 'response' AS reporting_year
 FROM public.dct
-LIMIT 100
+WHERE (raw ->> 'facilityPosted')::boolean = TRUE
+GROUP BY account_id, raw -> 'results' -> 'answers' -> 'reportingyear' ->> 'response'
+ORDER BY 1 DESC
