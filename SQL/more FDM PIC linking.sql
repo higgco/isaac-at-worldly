b989afc7f3cc4105449a9c3fa2eef1bc909f5842
Isaac Hopwood <isaac.hopwood@higg.com>
2025-07-09 16:20:17 -0500

    Update more FDM PIC linking.sql

--- SQL/more FDM PIC linking.sql
+++ SQL/more FDM PIC linking.sql
@@ -1,5 +1,51 @@
 SELECT
 	account_id,
+	CASE WHEN (
+	raw -> 'results' -> 'answers' -> 'sipfacilitytype' ->> 'response' ILIKE '%finalProductAssembly%'
+	AND 
+		CASE WHEN raw ->> 'status' ILIKE '%VRF%'
+			THEN (raw -> 'results' -> 'verifierCalculations' ->> 'finalProductAssembly_normalized_mj')::numeric > 0
+			ELSE (raw -> 'results' -> 'facilityCalculations' ->> 'finalProductAssembly_normalized_mj')::numeric > 0
+		END
+	AND 
+		CASE WHEN raw ->> 'status' ILIKE '%VRF%'
+			THEN
+				CASE WHEN raw ->> 'surveyVersion' ILIKE '%2.0.0%'
+					THEN (raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantfinalProductAssembly' ->> 'corrected')::numeric > 0
+					WHEN raw ->> 'surveyVersion' ILIKE '%3.0.0%'
+					THEN (raw -> 'results' -> 'answers' -> 'sipfacilityannualprodvolquantfinalProductAssembly' ->> 'corrected')::numeric > 0
+				END
+			ELSE 
+				CASE WHEN raw ->> 'surveyVersion' ILIKE '%2.0.0%'
+					THEN (raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantfinalProductAssembly' ->> 'response')::numeric > 0
+					WHEN raw ->> 'surveyVersion' ILIKE '%3.0.0%'
+					THEN (raw -> 'results' -> 'answers' -> 'sipfacilityannualprodvolquantfinalProductAssembly' ->> 'response')::numeric > 0	
+				END
+		END
+	) THEN TRUE
+	WHEN(
+	raw -> 'results' -> 'answers' -> 'sipfacilitytype' ->> 'response' ILIKE '%printingProductDyeingAndLaundering%'
+	AND 
+		CASE WHEN raw ->> 'status' ILIKE '%VRF%'
+			THEN (raw -> 'results' -> 'verifierCalculations' ->> 'printingProductDyeingAndLaundering_normalized_mj')::numeric > 0
+			ELSE (raw -> 'results' -> 'facilityCalculations' ->> 'printingProductDyeingAndLaundering_normalized_mj')::numeric > 0
+		END
+	AND 
+		CASE WHEN raw ->> 'status' ILIKE '%VRF%'
+			THEN
+				CASE WHEN raw ->> 'surveyVersion' ILIKE '%2.0.0%'
+					THEN (raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantprintingProductDyeingAndLaundering' ->> 'corrected')::numeric > 0
+					WHEN raw ->> 'surveyVersion' ILIKE '%3.0.0%'
+					THEN (raw -> 'results' -> 'answers' -> 'sipfacilityannualprodvolquantprintingProductDyeingAndLaundering' ->> 'corrected')::numeric > 0
+				END
+			ELSE 
+				CASE WHEN raw ->> 'surveyVersion' ILIKE '%2.0.0%'
+					THEN (raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantprintingProductDyeingAndLaundering' ->> 'response')::numeric > 0
+					WHEN raw ->> 'surveyVersion' ILIKE '%3.0.0%'
+					THEN (raw -> 'results' -> 'answers' -> 'sipfacilityannualprodvolquantprintingProductDyeingAndLaundering' ->> 'response')::numeric > 0	
+				END
+		END
+	) THEN TRUE ELSE FALSE END AS tier1_qualify,
 	raw
 FROM public.dct
 WHERE
@@ -11,28 +57,3 @@ WHERE
 	)
 	AND raw ->> 'status' ILIKE '%VRF'
 LIMIT 100
-
-CASE WHEN raw ->> 'surveyVersion' ILIKE '%2.0.0%' THEN
-	CASE WHEN (
-		raw -> 'results' -> 'answers' -> 'sipfacilitytype' ->> 'response' ILIKE '%finalProductAssembly%'
-		AND 
-			CASE WHEN raw ->> 'status' ILIKE '%VRF%'
-				THEN (raw -> 'results' -> 'verifierCalculations' ->> 'finalProductAssembly_normalized_mj')::numeric > 0
-				ELSE (raw -> 'results' -> 'facilityCalculations' ->> 'finalProductAssembly_normalized_mj')::numeric > 0
-			END
-		AND 
-			CASE WHEN raw ->> 'status' ILIKE '%VRF%'
-				THEN (raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantfinalProductAssembly' ->> 'corrected')::numeric > 0
-				ELSE (raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantfinalProductAssembly' ->> 'response')::numeric > 0
-		
-
-finalProductAssembly_normalized_mj
-printingProductDyeingAndLaundering_normalized_mj
-
-2.0.0:
-sipfacilityprodvolquantfinalProductAssembly
-sipfacilityprodvolquantprintingProductDyeingAndLaundering
-
-3.0.0
-sipfacilityannualprodvolquantfinalProductAssembly
-sipfacilityannualprodvolquantprintingProductDyeingAndLaundering
