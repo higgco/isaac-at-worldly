f220335a032e03c7a4b1bb0ecba76e2ec435f63f
Isaac Hopwood <isaac.hopwood@higg.com>
2025-07-24 11:53:44 -0500

    Update PIC cotton trust.sql
    added processes per account

--- PIC cotton trust.sql
+++ PIC cotton trust.sql
@@ -16,8 +16,25 @@ WITH cte AS (
   WHERE a.demo = FALSE AND a.active = TRUE
 )
 
-SELECT
-    COUNT(DISTINCT msi_id) AS count_id,
-    COUNT(DISTINCT account_id) AS count_account_id
-FROM cte
-WHERE all_processes ILIKE '%PR0804000852%';
+-- Count of MSI IDs and Account IDs
+-- SELECT
+--     COUNT(DISTINCT msi_id) AS count_id,
+--     COUNT(DISTINCT account_id) AS count_account_id
+-- FROM cte
+-- WHERE all_processes ILIKE '%PR0804000852%';
+
+
+-- Count MSI IDs per account first, then count how many accounts fall into each count
+SELECT 
+    msi_per_account.count_msi_ids,
+    COUNT(*) AS num_accounts
+FROM (
+    SELECT 
+        account_id,
+        COUNT(DISTINCT msi_id) AS count_msi_ids
+    FROM cte
+    WHERE all_processes ILIKE '%PR0804000852%'
+    GROUP BY account_id
+) AS msi_per_account
+GROUP BY msi_per_account.count_msi_ids
+ORDER BY msi_per_account.count_msi_ids;
