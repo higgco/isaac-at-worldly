1fea96cc3e385fdf1c4d653000453c4b6c3e5ebe
Isaac Hopwood <isaac.hopwood@higg.com>
2025-07-24 14:19:25 -0500

    Update PIC cotton trust.sql

--- PIC cotton trust.sql
+++ PIC cotton trust.sql
@@ -17,23 +17,24 @@ WITH cte AS (
 )
 
 -- Count of MSI IDs and Account IDs
--- SELECT
---     COUNT(DISTINCT msi_id) AS count_id,
---     COUNT(DISTINCT account_id) AS count_account_id
--- FROM cte
--- WHERE all_processes ILIKE '%PR0804000852%';
-
--- Accounts with MSI IDs that include the specified process
 SELECT
-    DISTINCT cte.account_id AS account_id,
-    a.name AS account_name,
-    COUNT(DISTINCT msi_id) AS count_id
+    COUNT(DISTINCT msi_id) AS count_id,
+    COUNT(DISTINCT account_id) AS count_account_id
 FROM cte
-LEFT JOIN public.account a ON a.account_id = cte.account_id
-WHERE all_processes ILIKE '%PR0804000852%'
-GROUP BY cte.account_id, a.name;
+WHERE all_processes ILIKE '%PR0804000852%';
 
+---
+-- Accounts with MSI IDs that include the specified process
+-- SELECT
+--     DISTINCT cte.account_id AS account_id,
+--     a.name AS account_name,
+--     COUNT(DISTINCT msi_id) AS count_id
+-- FROM cte
+-- LEFT JOIN public.account a ON a.account_id = cte.account_id
+-- WHERE all_processes ILIKE '%PR0804000852%'
+-- GROUP BY cte.account_id, a.name;
 
+---
 -- Count MSI IDs per account first, then count how many accounts fall into each count
 -- SELECT 
 --     msi_per_account.count_msi_ids,
