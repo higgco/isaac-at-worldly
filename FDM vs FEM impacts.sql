bb4e207f988e32cc9d26987b3144f690e21f97c9
Isaac Hopwood <isaac.hopwood@higg.com>
2025-07-28 10:27:47 -0500

    Update FDM vs FEM impacts.sql

--- FDM vs FEM impacts.sql
+++ FDM vs FEM impacts.sql
@@ -6,17 +6,6 @@ with fdm24 AS (
         fdm.raw -> 'results' -> 'answers' -> 'sipfacilitytype' ->> 'response' AS facility_types,
         fdm.raw -> 'results' -> 'answers' -> 'reportingmonth' ->> 'response' AS reporting_month,
         fdm.raw -> 'results' -> 'answers' -> 'reportingyear' ->> 'response' AS reporting_year,
-        -- CASE 
-        --     WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
-        --         THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'finalProductAssembly_normalized_mj')::numeric
-        --     ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'finalProductAssembly_normalized_mj')::numeric
-        -- END AS finalProductAssembly_normalized_mj,
-        -- CASE 
-        --     WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
-        --         THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'printingProductDyeingAndLaundering_normalized_mj')::numeric
-        --     ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'printingProductDyeingAndLaundering_normalized_mj')::numeric
-        -- END AS printingProductDyeingAndLaundering_normalized_mj,
-        ---
         CASE 
             WHEN fdm.raw ->> 'surveyVersion' ILIKE '%2.0.0%'
                 THEN (fdm.raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantfinalProductAssembly' ->> 'response')::numeric
@@ -43,16 +32,76 @@ with fdm24 AS (
         END AS materialProduction_prodvol,
         CASE 
             WHEN fdm.raw ->> 'surveyVersion' ILIKE '%2.0.0%'
-                THEN (fdm.raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantfdm.rawMaterialProcessing' ->> 'response')::numeric
+                THEN (fdm.raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantrawMaterialProcessing' ->> 'response')::numeric
             WHEN fdm.raw ->> 'surveyVersion' ILIKE '%3.0.0%'
-                THEN (fdm.raw -> 'results' -> 'answers' -> 'sipfacilityannualprodvolquantfdm.rawMaterialProcessing' ->> 'response')::numeric
-        END AS fdm.rawMaterialProcessing_prodvol,
+                THEN (fdm.raw -> 'results' -> 'answers' -> 'sipfacilityannualprodvolquantrawMaterialProcessing' ->> 'response')::numeric
+        END AS rawMaterialProcessing_prodvol,
         CASE 
             WHEN fdm.raw ->> 'surveyVersion' ILIKE '%2.0.0%'
-                THEN (fdm.raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantfdm.rawMaterialCollection' ->> 'response')::numeric
+                THEN (fdm.raw -> 'results' -> 'answers' -> 'sipfacilityprodvolquantrawMaterialCollection' ->> 'response')::numeric
             WHEN fdm.raw ->> 'surveyVersion' ILIKE '%3.0.0%'
-                THEN (fdm.raw -> 'results' -> 'answers' -> 'sipfacilityannualprodvolquantfdm.rawMaterialCollection' ->> 'response')::numeric
-        END AS fdm.rawMaterialCollection_prodvol,
+                THEN (fdm.raw -> 'results' -> 'answers' -> 'sipfacilityannualprodvolquantrawMaterialCollection' ->> 'response')::numeric
+        END AS rawMaterialCollection_prodvol,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'finalProductAssembly_mj')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'finalProductAssembly_mj')::numeric
+        END AS finalProductAssembly_mj,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'printingProductDyeingAndLaundering_mj')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'printingProductDyeingAndLaundering_mj')::numeric
+        END AS printingProductDyeingAndLaundering_mj,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'hardComponentTrimProduction_mj')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'hardComponentTrimProduction_mj')::numeric
+        END AS hardComponentTrimProduction_mj,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'materialProduction_mj')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'materialProduction_mj')::numeric
+        END AS materialProduction_mj,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'rawMaterialProcessing_mj')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'rawMaterialProcessing_mj')::numeric
+        END AS rawMaterialProcessing_mj,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'rawMaterialCollection_mj')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'rawMaterialCollection_mj')::numeric
+        END AS rawMaterialCollection_mj,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'finalProductAssembly_total_kgco2e')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'finalProductAssembly_total_kgco2e')::numeric
+        END AS finalProductAssembly_total_kgco2e,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'printingProductDyeingAndLaundering_total_kgco2e')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'printingProductDyeingAndLaundering_total_kgco2e')::numeric
+        END AS printingProductDyeingAndLaundering_total_kgco2e,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'hardComponentTrimProduction_total_kgco2e')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'hardComponentTrimProduction_total_kgco2e')::numeric
+        END AS hardComponentTrimProduction_total_kgco2e,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'materialProduction_total_kgco2e')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'materialProduction_total_kgco2e')::numeric
+        END AS materialProduction_total_kgco2e,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'rawMaterialProcessing_total_kgco2e')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'rawMaterialProcessing_total_kgco2e')::numeric
+        END AS rawMaterialProcessing_total_kgco2e,
+        CASE 
+            WHEN fdm.raw ->> 'status' ILIKE '%VRF%'
+                THEN (fdm.raw -> 'results' -> 'verifierCalculations' ->> 'rawMaterialCollection_total_kgco2e')::numeric
+            ELSE (fdm.raw -> 'results' -> 'facilityCalculations' ->> 'rawMaterialCollection_total_kgco2e')::numeric
+        END AS rawMaterialCollection_total_kgco2e
         -- fdm.raw
     FROM 
         public.dct AS fdm
@@ -62,6 +111,5 @@ with fdm24 AS (
         (fdm.raw ->> 'facilityPosted')::boolean = TRUE
         AND fdm.raw ->> 'status' NOT ILIKE '%ASD%'
         AND fdm.raw -> 'results' -> 'answers' -> 'reportingyear' ->> 'response' LIKE '2024'
-    GROUP BY 2,3
     LIMIT 1000
 )
