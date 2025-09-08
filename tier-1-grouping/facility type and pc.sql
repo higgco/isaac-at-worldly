b85f95c8896153679fc2a019d488326dff41a8e1
Isaac Hopwood <isaac.hopwood@higg.com>
2025-09-08 17:37:42 -0500

    Update facility type and pc.sql

--- tier-1-grouping/facility type and pc.sql
+++ tier-1-grouping/facility type and pc.sql
@@ -12,7 +12,9 @@ SELECT
     performance -> 'sipfacilitytype' AS sipfacilitytype,
     jsonb_array_length(performance -> 'sipfacilitytype') AS facility_type_count,
     performance -> 'sipproductcategories' AS sipproductcategories,
-    jsonb_array_length(performance -> 'sipproductcategories') AS product_category_count
+    jsonb_array_length(performance -> 'sipproductcategories') AS product_category_count,
+    performance -> 'sipfacilityapparelpc' AS sipfacilityapparelpc,
+    jsonb_array_length(performance -> 'sipfacilityapparelpc') AS apparel_pc_count
 FROM fem_simple_090825
 WHERE rfi_pid = 'fem2024'
     AND performance ->> 'sipfacilitytype' ILIKE '%Assembler%'
@@ -20,16 +22,22 @@ WHERE rfi_pid = 'fem2024'
 )
 
 SELECT 
-    COUNT(assessment_id) AS total_assessments
+    -- COUNT(
+        DISTINCT assessment_id
+        -- ) AS total_assessments,
+        AS assessment_id,
     -- ROUND(
     --     (COUNT(assessment_id) * 100.0) / SUM(COUNT(assessment_id)) OVER(), 
     --     2
     -- ) AS percentage_of_total,
     -- facility_type_count,
     -- product_category_count
+    sipfacilityapparelpc,
+    apparel_pc_count
+    -- DISTINCT jsonb_array_elements_text(sipfacilityapparelpc) AS distinct_apparel_pc_values
 FROM cte
 WHERE
     facility_type_count = 1
     AND product_category_count = 1
--- GROUP BY 3, 4
+-- GROUP BY 3
 ORDER BY 1 DESC;
