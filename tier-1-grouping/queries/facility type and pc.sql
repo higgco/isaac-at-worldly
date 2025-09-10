eb865e0e1ef7e6f1b44b39113c88f35caaa7664c
Isaac Hopwood <isaac.hopwood@higg.com>
2025-09-10 11:11:31 -0500

    add energy outlier to query

--- tier-1-grouping/queries/facility type and pc.sql
+++ tier-1-grouping/queries/facility type and pc.sql
@@ -8,20 +8,30 @@
 
 WITH cte AS (
 SELECT 
-    assessment_id,
-    performance -> 'sipfacilitytype' AS sipfacilitytype,
-    jsonb_array_length(performance -> 'sipfacilitytype') AS facility_type_count,
-    (performance -> 'finalProductAssemblytotal')::numeric AS finished_product_assembly_prod_vol_pcs,
-    performance -> 'sipproductcategories' AS sipproductcategories,
-    jsonb_array_length(performance -> 'sipproductcategories') AS product_category_count,
-    performance -> 'sipfacilityapparelpc' AS sipfacilityapparelpc,
-    jsonb_array_length(performance -> 'sipfacilityapparelpc') AS apparel_pc_count,
-    (performance -> 'ensourcetotal')::numeric AS total_energy_mj,
-    (performance -> 'totalGHGemissions')::numeric AS totalGHGemissions
-FROM fem_simple_090825
+    fem.assessment_id,
+    fem.performance -> 'sipfacilitytype' AS sipfacilitytype,
+    jsonb_array_length(fem.performance -> 'sipfacilitytype') AS facility_type_count,
+    (fem.performance -> 'finalProductAssemblytotal')::numeric AS finished_product_assembly_prod_vol_pcs,
+    fem.performance -> 'sipproductcategories' AS sipproductcategories,
+    jsonb_array_length(fem.performance -> 'sipproductcategories') AS product_category_count,
+    fem.performance -> 'sipfacilityapparelpc' AS sipfacilityapparelpc,
+    jsonb_array_length(fem.performance -> 'sipfacilityapparelpc') AS apparel_pc_count,
+    (fem.performance -> 'ensourcetotal')::numeric AS total_energy_mj,
+    (fem.performance -> 'totalGHGemissions')::numeric AS totalGHGemissions,
+    CASE WHEN (
+        outliers.total_mj_outlier = TRUE
+        OR outliers.total_mj_facility_type_outlier_ = TRUE
+        OR outliers.normalized_mj_facility_type_outlier_ = TRUE
+        OR outliers.year_over_year_mj_outlier = TRUE
+    ) THEN TRUE
+    ELSE FALSE
+    END AS energy_outlier
+FROM fem_simple_090825 AS fem
+LEFT JOIN fem_data_outliers AS outliers
+ON fem.assessment_id = outliers.assessment_id
 WHERE rfi_pid = 'fem2024'
-    AND performance ->> 'sipfacilitytype' ILIKE '%Assembler%'
-    AND performance ->> 'sipproductcategories' ILIKE '%Apparel%'
+    AND fem.performance ->> 'sipfacilitytype' ILIKE '%Assembler%'
+    AND fem.performance ->> 'sipproductcategories' ILIKE '%Apparel%'
 )
 
 SELECT 
@@ -39,7 +49,8 @@ SELECT
     sipfacilityapparelpc,
     apparel_pc_count,
     total_energy_mj,
-    totalGHGemissions
+    totalGHGemissions,
+    energy_outlier
     -- DISTINCT jsonb_array_elements_text(sipfacilityapparelpc) AS distinct_apparel_pc_values
 FROM cte
 WHERE
