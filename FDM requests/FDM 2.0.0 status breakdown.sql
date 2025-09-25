d8158c81bd7ed75fc03ff260f411abe371e81877
Isaac Hopwood <isaac.hopwood@higg.com>
2025-09-25 11:51:01 -0500

    Create FDM 2.0.0 status breakdown.sql

---
+++ FDM requests/FDM 2.0.0 status breakdown.sql
@@ -0,0 +1,11 @@
+SELECT
+COUNT(DISTINCT dct.assessment_id) AS total_assessments,
+dct.raw ->> 'status' AS status
+FROM public.dct AS dct
+INNER JOIN public.account AS a ON a.account_id = dct.account_id
+WHERE
+    (dct.raw ->> 'facilityPosted')::boolean = TRUE
+    AND dct.raw ->> 'surveyVersion' = '2.0.0'
+    AND a.demo = FALSE
+    AND dct.raw ->> 'status' != 'ASD'
+GROUP BY dct.raw ->> 'status'
